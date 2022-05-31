c8fe46578987fe6c7d3e2689329e3bd56520a825
Nick Wright <nicholaswrightwork@gmail.com>
2022-05-31 03:18:08 -0400

    Clean fire.js

--- src/fire.js
+++ src/fire.js
@@ -1,23 +1,12 @@
-// Import the functions you need from the SDKs you need
+// Import FirebaseConfig to use to initialize app
 import config from "./config";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: config.APIKEY,
-  authDomain: config.AUTHDOMAIN,
-  projectId: config.PROJECTID,
-  storageBucket: config.STORAGEBUCKET,
-  messagingSenderId: config.MESSAGINGSENDERID,
-  appId: config.APPID
-};
 
-// Initialize Firebase,. Whole website should crash here if Firebase initialize did not work.
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase,Auth,Database.
+const app = initializeApp(config);
 const auth= getAuth(app);
 const database = getDatabase(app);
 
